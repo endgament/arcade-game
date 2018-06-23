d7e2cd7c5410774e8df2a0773aeb8284ac80f01b
Konstantina Tsarvouli <ntina_t@hotmail.com>
2018-06-23 10:12:29 +0300

    Adding constructor functions for player and enemy

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x,y,sprite) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -24,12 +21,19 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (x,y,sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = "images/char-princess-girl";
+};
+
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+allEnemies = [];
+player = ;
 
 
 // This listens for key presses and sends the keys to your
