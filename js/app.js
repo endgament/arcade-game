bfd8843ff2125f961f3b83881671f2acc366ce6b
Konstantina Tsarvouli <ntina_t@hotmail.com>
2018-06-23 10:18:34 +0300

    Fixing the constructor function for player and enemies

--- js/app.js
+++ js/app.js
@@ -32,8 +32,8 @@ var Player = function (x,y,sprite) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-allEnemies = [];
-player = ;
+//allEnemies = [];
+//player = ;
 
 
 // This listens for key presses and sends the keys to your
