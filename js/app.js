48d50181085a245c516c4294ee93fcb4710be9b2
Konstantina Tsarvouli <ntina_t@hotmail.com>
2018-07-18 10:27:50 +0300

    Update player's render function

--- js/app.js
+++ js/app.js
@@ -79,10 +79,8 @@ class Player {
         }
     };
 
-    render(){
-        function() {
-            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
 
     handleInput(key) {
